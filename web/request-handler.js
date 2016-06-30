fdd161b2dc121cb2ef09fe2664ba180d6783c652
juliusbuckley <juliusbuckley@gmail.com>
2016-06-29 17:34:34 -0700

    Completed 1st test prompt.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,41 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+var helpers = require('./http-helpers');
+var url = require('url');
+var fs = require('fs');
+
+var actions = {
+  'GET': function(request, response) {
+
+    // only do this when page loads  
+    fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', function(err, html) {
+      response.writeHead(200, helpers.headers);
+      response.end(html);
+    });
+
+  }, 
+  'POST': function(request, response) {
+
+
+
+    sendResponse(response, '', 201);
+  },
+  'OPTIONS': function(request, response) {
+
+    sendResponse(response, null);
+  }
+};
+
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  console.log('req.url', req.url);
+
+  var action = actions[req.method];
+  if (action) {
+    action(req, res);
+  } else {
+    sendResponse(res, '', 404);
+  }
+
 };
