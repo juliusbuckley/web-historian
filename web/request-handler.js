28442d095f3c73c4c482c54a1d91eca89115ad96
juliusbuckley <juliusbuckley@gmail.com>
2016-06-29 23:45:35 -0700

    Completed 3/4 request-handler prompts.

--- web/request-handler.js
+++ web/request-handler.js
@@ -5,7 +5,7 @@ var url = require('url');
 var fs = require('fs');
 
 
-var render = function (request, response, assets, route) {
+var init = function (request, response, assets, route) {
   fs.readFile(assets + route, 'utf8', function(err, html) { 
     if (err) {
       response.writeHead(404, helpers.headers);
@@ -17,6 +17,22 @@ var render = function (request, response, assets, route) {
   });
 };
 
+var send = function (request, response, assets, route) {
+  console.log(assets + route);
+  fs.readFile(assets + route, 'utf8', function(err, data) { 
+    if (err) {
+      console.log('errrrroor');
+      response.writeHead(404, helpers.headers);
+      response.end();
+    } else {
+      response.writeHead(200, helpers.headers);
+      console.log(data);
+      response.end(data);
+    }
+  });
+};
+
+
 var routes = {
   '/': '/index.html',
   '/web/public/index.html': '/index.html',
@@ -26,20 +42,34 @@ var routes = {
 
 var actions = {
   'GET': function(request, response) {
+
+    console.log('archive.paths.siteAssets: ', archive.paths.siteAssets, 'requestURL: ', request.url);
+    
   
     if (routes[request.url]) {
-      render(request, response, archive.paths.siteAssets, routes[request.url]);
+      init(request, response, archive.paths.siteAssets, routes[request.url]);
     } else {
-
+      send(request, response, archive.paths.archivedSites, request.url);
     }
 
 
   }, 
   'POST': function(request, response) {
+    console.log('in post');
+    var data = '';
+    request.on('data', function(chunk) {
+      data += chunk.toString();
+    });
+    request.on('end', function() {
+      //console.log('DATA', data.split('=')[1]);
+      var text = data.split('=')[1];
+      fs.appendFile(archive.paths.list, text, function(err) {});
+      response.writeHead(201, helpers.headers);
+      request.end();
+    });
+    
 
-
-
-    sendResponse(response, '', 201);
+    
   },
   'OPTIONS': function(request, response) {
 
@@ -49,9 +79,9 @@ var actions = {
 
 exports.handleRequest = function (req, res) {
 
-  //console.log('req.url', req.url);
-  var parsedUrl = url.parse(req.url);
-  //console.log('hostname', parsedUrl);
+  // console.log('req.url', req.url);
+  // var parsedUrl = url.parse(req.url);
+  // console.log('hostname', parsedUrl.pathname);
 
   var action = actions[req.method];
   if (action) {
